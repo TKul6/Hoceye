e0a774af130316adb71017065be2a62b0e47eba7
TMaster <github321>
2016-06-12 12:18:11 +0300

    Initial commit, try to build the PathConstructor using TDD

---
+++ HocEye.Core/PathConstructor.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Operations;
+
+namespace HocEye.Core
+{
+    public class PathConstructor
+    {
+        private ITextStructureNavigator _textNavigator;
+
+
+
+        public PathConstructor(ITextStructureNavigator textNavigator)
+        {
+            _textNavigator = textNavigator;
+        }
+
+        public string ConstructPathBackwards(ITextSnapshot currentSnapshot, int position)
+        {
+            StringBuilder pathBuilder = new StringBuilder();
+
+            return InnerConstructPathBackwards(currentSnapshot, position, pathBuilder);
+
+
+        }
+
+        private string InnerConstructPathBackwards(ITextSnapshot currentSnapshot, int position, StringBuilder builder)
+        {
+
+            var currentNameSnapshot = _textNavigator.GetExtentOfWord(new SnapshotPoint(currentSnapshot, position));
+
+            builder.Insert(0, currentNameSnapshot.Span.GetText());
+
+            var nextPosition = currentNameSnapshot.Span.Start.Position - 2;
+
+            if (nextPosition > 0)
+            {
+                builder.Insert(0, ".");
+
+                InnerConstructPathBackwards(currentSnapshot, nextPosition, builder);
+            }
+
+            return builder.ToString();
+
+        }
+    }
+}
---
+++ Hoceye.Core.Tests/PathConstructorTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HocEye.Core;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Operations;
+using Moq;
+using NUnit.Framework;
+
+namespace Hoceye.Core.Tests
+{
+    [TestFixture(Author = "Tomer K", Category = "Hoceye.Core")]
+    public class PathConstructorTests
+    {
+        [Test(Description = "Validate simple path construction, when the all the path elements exist in the same line")]
+        [TestCase("application.prod.resources.mongo.connection",",application.prod.resources.mongo")]
+        public void When_Constucting_Elment_Path(string rawLine, string excpectedPath)
+        {
+            //Act
+
+            var textNavigator = new Mock<ITextStructureNavigator>();
+
+            textNavigator.Setup(t => t.GetExtentOfWord(It.IsAny<SnapshotPoint>())).Returns((SnapshotPoint point) =>
+            {
+                var snapshot = point.Snapshot;
+
+             
+
+                var end = rawLine.LastIndexOf('.');
+                var start = rawLine.LastIndexOf('.', 0, end - 1);
+
+                var snapshotSpan = new SnapshotSpan(snapshot,start,end);
+                
+
+                var textExtent = new TextExtent(snapshotSpan, false);
+
+                return textExtent;
+                
+            });
+
+            
+            
+
+
+
+            var textSnapshot = new Mock<ITextSnapshot>();
+
+            textSnapshot.SetupGet(snapshot => snapshot.Length).Returns(rawLine.Length);
+
+            
+            var constructor = new PathConstructor(textNavigator.Object);
+
+            //Act
+
+            var pathResult = constructor.ConstructPathBackwards(textSnapshot.Object,excpectedPath.Length-3);
+
+            //Assert
+
+            Assert.That(pathResult, Is.EqualTo(excpectedPath));
+
+        }
+    }
+}
